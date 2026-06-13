f9dc0290984ebde6474fd33ff8fd2e14c7eae58b
Andrea Difino <adifino13@gmail.com>
2026-06-13 17:06:39 +0200

    Update transcribe.py

--- safewave/services/transcribe.py
+++ safewave/services/transcribe.py
@@ -9,7 +9,9 @@ class TranscribeService:
         self.full_text: str
 
     def transcribe_audio(self, audio_path: str):
-        segments, _ = self.model.transcribe(audio_path, beam_size=5, word_timestamps=True)
+        style_prompt = "mario.rossi@gmail.com. info@azienda.it. test@hotmail.com."
+
+        segments, _ = self.model.transcribe(audio_path, beam_size=5, word_timestamps=True, initial_prompt=style_prompt)
         words_list: list[str] = []
         intervals_list: list[str] = []
 
