049ea91b48e8b6ebdbeab2c461434ceb8f48b2d3
Andrea Difino <adifino13@gmail.com>
2026-06-14 11:41:33 +0200

    Added first two tests

---
+++ tests/unit/utils/test_audio_censor.py
@@ -0,0 +1,74 @@
+import os
+import math
+import pytest
+from pydub import AudioSegment
+from pydub.generators import Sine
+
+from safewave.utils.audio_censor import AudioCensor, AudioMaskTypes
+
+class TestAudioCensor:
+
+    @pytest.fixture
+    def audio_censor(self, tmp_path):
+        """tmp_path for the censored test audio"""
+        test_dir = str(tmp_path / "censored_test_output")
+        return AudioCensor(rel_output_dir=test_dir)
+
+    @pytest.fixture
+    def dummy_audio_file(self, tmp_path):
+        """
+        Dummy audio file for test purpose
+        """
+
+        audio = Sine(440).to_audio_segment(duration=5000) 
+        file_path = str(tmp_path / "dummy_input.wav")
+
+        audio.export(file_path, format="wav") 
+        return file_path
+
+    @pytest.mark.parametrize("mode, expected_dbfs", [
+        (AudioMaskTypes.SILENCE, float('-inf')),
+        (AudioMaskTypes.BEEP, -18.01)
+    ])
+
+    def test_censor_file_applies_correct_mask(self, audio_censor, dummy_audio_file, mode, expected_dbfs):
+        """
+        Verify if censor is correctly applied to the audio
+        """
+
+        start_sec, end_sec = 1.0, 2.0
+        timestamps = [(start_sec, end_sec)]
+
+        output_path = audio_censor.censor_file(
+            input_path=dummy_audio_file, 
+            timestamps=timestamps, 
+            mode=mode
+        )
+        
+        assert os.path.exists(output_path), "Censor file was not created"
+
+        result_audio = AudioSegment.from_file(output_path)
+
+        start_ms = int(start_sec * 1000)
+        end_ms = int(end_sec * 1000)
+
+        safe_margin = 50 
+        censored_segment = result_audio[start_ms + safe_margin : end_ms - safe_margin]
+        uncensored_segment = result_audio[0 : start_ms - safe_margin]
+
+        
+        if mode == AudioMaskTypes.SILENCE:
+            assert censored_segment.dBFS == expected_dbfs, "The segment is not completely silence"
+        elif mode == AudioMaskTypes.BEEP:
+            assert math.isclose(censored_segment.dBFS, expected_dbfs, abs_tol=0.5), \
+                f"The beep shoul be at {expected_dbfs} dBFS, but it is at {censored_segment.dBFS}"
+
+        assert uncensored_segment.dBFS != censored_segment.dBFS, \
+            "It seems that the outer audio have been modified"
+        
+    def test_enum_value(self):
+        
+        assert ["BEEP", "SILENCE"] == AudioMaskTypes._member_names_
+        
+        assert AudioMaskTypes.BEEP.value == "beep"
+        assert AudioMaskTypes.SILENCE.value == "silence"
---
+++ tests/unit/utils/test_audio_manager.py
@@ -0,0 +1,47 @@
+import pytest
+from safewave.utils.audio_manager import IOAudioManager
+from pydub.generators import Sine
+
+class TestAudioManager:
+
+    @pytest.fixture
+    def test_audio_dir(self, tmp_path):
+        """
+        Create the tmp path for the test
+        """
+        target_dir = tmp_path / "audio_test_output"
+        target_dir.mkdir(exist_ok=True)
+        return target_dir
+
+    @pytest.fixture
+    def audio_manager(self, test_audio_dir):
+        """
+        Initializa manager with the tmp path
+        """
+        return IOAudioManager(audio_path=str(test_audio_dir))
+    
+    @pytest.fixture
+    def dummy_audio_files(self, test_audio_dir):
+        file_paths = []
+        
+        for i in range(1, 4):
+            file_path = test_audio_dir / f"dummy_input{i}.wav"
+            
+            audio = Sine(440).to_audio_segment(duration=100)
+            audio.export(str(file_path), format="wav") 
+            
+            file_paths.append(file_path) 
+            
+        return file_paths
+    
+    def test_get_audio(self, audio_manager, dummy_audio_files):
+        files = audio_manager.get_audio()
+
+        assert sorted(files) == sorted(dummy_audio_files), "The manager didn't find the correct files"
+
+    def test_supported_extensions(self, audio_manager):
+        difference = audio_manager.SUPPORTED_EXTENSIONS.difference({'.mp3', '.wav', '.flac', '.m4a', '.ogg'})
+
+        assert len(difference) == 0, "Supported extensions missmatch"
+
+        
