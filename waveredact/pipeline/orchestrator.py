b7dc560769bccbca388e917d293131453ffc6dd2
Andrea Difino <80335070+Andrea-Difino@users.noreply.github.com>
2026-07-10 16:50:38 +0200

    Added docstrings to orchestrator

--- waveredact/pipeline/orchestrator.py
+++ waveredact/pipeline/orchestrator.py
@@ -1,17 +1,28 @@
-from pathlib import Path
 from waveredact.pipeline.mapper import ChunkMapper
 from waveredact.pipeline.privacy_pipeline import DataPrivacyPipeline
 import logging
+from typing import Dict, Set
 
 logger = logging.getLogger(__name__)
 FORMAT = "%(asctime)s %(message)s"
 
 
 class Orchestrator:
+    """
+        Use the DataPrivacyPipeline class to orchestrate all the programm. Handle the user_interaction and return the ids of the sensitive words.
+
+        Attributes:
+            index_word_pair     - Dict used to map each index to the corrisponding word
+            mappers             - List of ChunkMapper. Every mapper contain important informations about each chunk
+            data_pipeline       - Use the extractors to extract the sensitive ids from the sentences
+            use_llm             - Parameter passed by command line used to activate or not the LLM step
+            interactive_mode    - Parameter passed by command line used to activate or not the human interaction
+    """
+
     def __init__(
         self,
         *,
-        index_word_pair: dict[int, str], 
+        index_word_pair: Dict[int, str], 
         mappers: list[ChunkMapper],
         data_pipeline: DataPrivacyPipeline,
         use_llm: bool = False,
@@ -26,8 +37,14 @@ class Orchestrator:
     def run_audio_chunks(
         self
     ) -> list[int]:
-        full_idx: set[int] = set()
-        full_locked_idx: set[int] = set()
+        """
+            Use Regex and GLiNER to censor data for each chunk of the audios
+
+            Return:
+                list of integers corresponding to the words to censor
+        """
+        full_idx: Set[int] = set()
+        full_locked_idx: Set[int] = set()
         
         chunk_ambiguous_list: list[list[int]] = []
 
@@ -69,8 +86,22 @@ class Orchestrator:
                 logger.info("Fast mode: LLM bypassed.\n")
                 return ordered_idx
         
-    def run_llm_extraction(self, chunk_ambiguous_list: list[list[int]], locked_idx: set[int]) -> list[int]:
-        checked_idx: set[int] = set()
+    def run_llm_extraction(
+        self, 
+        chunk_ambiguous_list: list[list[int]], 
+        locked_idx: Set[int]
+    ) -> list[int]:
+        """
+            Use LLM to check the answers given by the GLiNER model and find missed sensitive words
+
+            Params:
+                chunk_ambiguous_list    - list of the indices for each chunk that had a confidence score lower than 0.99 in the GLiNER step
+                locked_idx              - set of the indices that must be censored because had a nearly 1.0 confidence score
+
+            Return:
+                list of the final sensitive indices
+        """
+        checked_idx: Set[int] = set()
         n_chunks = len(self.mappers)
 
         for i in range(n_chunks):
@@ -89,6 +120,16 @@ class Orchestrator:
         return sorted(checked_idx)
     
     def _human_approval(self, sensitive_words: list[str]) -> bool:
+        """
+            Function used for human approval in the interactive_mode
+
+            Params:
+                sensitive_words - list of the sensitive words found until now
+
+            Return:
+                True if the user is satisfied and want to end the programm or continue to the next audio
+                False if he is not satisfied and wants to use the LLM
+        """
         while True:
             user_question = input(
                 f"\nThese are the words found:\n{sensitive_words}\n\nAre they all (Y/N)? "
